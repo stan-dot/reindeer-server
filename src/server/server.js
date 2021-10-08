9888151585fb3be31415374ed3d13b16d6e3f9c4
stan-dot <56644812+stan-dot@users.noreply.github.com>
2021-10-08 18:38:23 +0100

    set up node env connection

--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,24 @@
 
-const express = require('express')
+import express  from 'express';
+import http from 'http';
+import {Server } from "socket.io"
 const app = express()
 const port = 3000
+const server = http.createServer(app);
+const io = new Server(server, {
+  pingInterval: 300, 
+  pingTimeout: 5000
+})
+io.on("connection", socket =>{
+  socket.send("hello, socket connection established")
+  console.log("connected to trhe client");
+})
+app.use(express.static("build"))
 
+app.use(express.static("public"))
 app.get('/', (req, res) => {
   res.send('Hello World!')
+  res.sendFile("./index.html")
 })
 
 app.listen(port, () => {
