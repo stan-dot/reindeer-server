4ec49b048dca0869a813f162b08ba4b4cd538517
stan-dot <56644812+stan-dot@users.noreply.github.com>
2021-10-08 17:23:19 +0100

    set up the server

---
+++ src/server/runScript.js
@@ -0,0 +1,52 @@
+const myPath = './src/server/scripts/script.py';
+const { spawn } = require('child_process')
+
+const logOutput = (name) => (message) => console.log(`[${name}] ${message}`)
+
+function run() {
+  return new Promise((resolve, reject) => {
+    const process = spawn('python', [myPath, 'my', 'args']);
+
+    const out = []
+    process.stdout.on(
+      'data',
+      (data) => {
+        out.push(data.toString());
+        logOutput('stdout')(data);
+      }
+    );
+
+    const err = []
+    process.stderr.on(
+      'data',
+      (data) => {
+        err.push(data.toString());
+        logOutput('stderr')(data);
+      }
+    );
+
+    process.on('exit', (code, signal) => {
+      logOutput('exit')(`${code} (${signal})`)
+      if (code !== 0) {
+        reject(new Error(err.join('\n')))
+        return
+      }
+      try {
+        resolve(JSON.parse(out[0]));
+      } catch(e) {
+        reject(e);
+      }
+    });
+  });
+}
+
+(async () => {
+  try {
+    const output = await run()
+    logOutput('main')(output.message)
+    process.exit(0)
+  } catch (e) {
+    console.error('Error during script execution ', e.stack);
+    process.exit(1);
+  }
+})();
